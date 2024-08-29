258dc7d4ab64cec06078234c2ab6d6ae5e5126c2
Vishwanath <vishwanath9216@gmail.com>
2024-08-29 10:32:20 +0530

    commit changes

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+import { app } from "./app.js";
+import connectDB from './db/index.js';
+
+
+dotenv.config({
+    path:'./.env'
+})
+
+const PORT = process.env.PORT || 3001
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`server is running on port ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection is secure ", err);
+    
+})
+
+
